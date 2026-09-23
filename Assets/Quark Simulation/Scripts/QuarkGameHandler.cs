682ec155c0787ed09a2bb1642f4f77656149275e
Fabian <wyssfabian1@gmail.com>
2026-09-23 15:46:49 +0200

    made it so you can choose whether you wanna go to the 2D or 3D simulation in the beginning

--- Assets/Quark Simulation/Scripts/QuarkGameHandler.cs
+++ Assets/Quark Simulation/Scripts/QuarkGameHandler.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class QuarkGameHandler : MonoBehaviour
 {
+    public Canvas UI3D;
     public CameraHandler3D cameraHandler3D;
     public Controller3D controller3D;
+    public Canvas UI2D;
     public CameraHandler2D cameraHandler2D;
     public Controller2D controller2D;
 
@@ -22,20 +24,24 @@ public class QuarkGameHandler : MonoBehaviour
     public void Start2D()
     {
         SelectionScreen.enabled = false;
+        UI2D.enabled = true;
         cameraHandler2D.enabled = true;
         controller2D.enabled = true;
     }
     public void Start3D()
     {
         SelectionScreen.enabled = false;
+        UI3D.enabled = true;
         cameraHandler3D.enabled = true;
         controller3D.enabled = true;
     }
     public void EndAll()
     {
         SelectionScreen.enabled = true;
+        UI2D.enabled = true;
         cameraHandler2D.enabled = false;
         controller2D.enabled = false;
+        UI3D.enabled = true;
         cameraHandler3D.enabled = false;
         controller3D.enabled = false;
     }
