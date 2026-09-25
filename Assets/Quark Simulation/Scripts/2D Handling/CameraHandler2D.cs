06849495ea1d0dd3d39184d9d47f2e0224fe11c3
Fabian <wyssfabian1@gmail.com>
2026-09-25 15:57:26 +0200

    Made it so you can use the scroll wheel to change how fast you move around

--- Assets/Quark Simulation/Scripts/2D Handling/CameraHandler2D.cs
+++ Assets/Quark Simulation/Scripts/2D Handling/CameraHandler2D.cs
@@ -1,25 +1,54 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CameraHandler2D : MonoBehaviour
 {
+    
+    public float MovementScale = 1;
+    public float MovementScaleFactor = 2;
+    public TMP_InputField MovementScaleInput;
     InputAction moveAction;
+    InputAction ScrollAction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
+        ScrollAction = InputSystem.actions.FindAction("Scroll");
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 moveValue = moveAction.ReadValue<Vector3>();
+        Vector2 scrollValue = ScrollAction.ReadValue<Vector2>();
+        if (scrollValue.y < 0)
+        {
+            MovementScale /= MovementScaleFactor;        
+            MovementScaleInput.text = MovementScale.ToString();
+        } else if(scrollValue.y > 0)
+        {
+            MovementScale *= MovementScaleFactor;
+            MovementScaleInput.text = MovementScale.ToString();   
+        }
 
-        transform.position += new Vector3(moveValue.x, moveValue.z,moveValue.y);
+        transform.position += new Vector3(moveValue.x, moveValue.z,moveValue.y) * MovementScale;
     }
     public void ResetCamera()
     {
         transform.position = new Vector3(0,0,-40);
         transform.eulerAngles = new Vector3(0,0,0);
+    }
+    public void setMovementScale()
+    {
+        float inp;
+        if (float.TryParse(MovementScaleInput.text, out inp))
+        {
+            MovementScale = inp;
+        }
+        else
+        {
+            MovementScaleInput.text = MovementScale.ToString();
+        }
     }
 }
--- Assets/Quark Simulation/Scripts/3D Handling/CameraHandler3D.cs
+++ Assets/Quark Simulation/Scripts/3D Handling/CameraHandler3D.cs
@@ -1,18 +1,25 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CameraHandler3D : MonoBehaviour
 {
     public float sensitivity = 1;
+    public float MovementScale = 1;
+    public float MovementScaleFactor = 2;
+
+    public TMP_InputField MovementScaleInput;
     InputAction moveAction;
     InputAction lookAction;
     InputAction LeftClickAction;
+    InputAction ScrollAction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         lookAction = InputSystem.actions.FindAction("Look");
         LeftClickAction = InputSystem.actions.FindAction("Click");
+        ScrollAction = InputSystem.actions.FindAction("Scroll");
     }
 
     // Update is called once per frame
@@ -20,16 +27,42 @@ public class CameraHandler3D : MonoBehaviour
     {
         Vector3 moveValue = moveAction.ReadValue<Vector3>();
         Vector2 lookValue = lookAction.ReadValue<Vector2>() * sensitivity;
+        Vector2 scrollValue = ScrollAction.ReadValue<Vector2>();
+        if (scrollValue.y < 0)
+        {
+            MovementScale /= MovementScaleFactor;        
+            MovementScaleInput.text = MovementScale.ToString();
+            //Debug.Log((scrollValue.y, MovementScale));      
+        } else if(scrollValue.y > 0)
+        {
+            MovementScale *= MovementScaleFactor;
+            MovementScaleInput.text = MovementScale.ToString();
+            //Debug.Log((scrollValue.y, MovementScale));      
+        }
+
         if (LeftClickAction.IsPressed())
         {
             transform.Rotate(new Vector2(-lookValue.y,lookValue.x),Space.Self);        
         }
 
-        transform.Translate(moveValue);
+        transform.Translate(moveValue * MovementScale);
     }
     public void ResetCamera()
     {
         transform.position = new Vector3(0,0,-40);
         transform.eulerAngles = new Vector3(0,0,0);
+    }
+
+    public void setMovementScale()
+    {
+        float inp;
+        if (float.TryParse(MovementScaleInput.text, out inp))
+        {
+            MovementScale = inp;
+        }
+        else
+        {
+            MovementScaleInput.text = MovementScale.ToString();
+        }
     }
 }
